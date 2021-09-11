1cce98e4f51a3c24aa0654391605e25bbb7fdcb3
MuursaleenBats <mursaleenbatatawala@gmail.com>
2021-09-11 18:38:25 +0530

    crud update

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -10,13 +10,25 @@ export class CrudService {
 
   public addMobpass(mobiPass)
   {
-    return this.http.post('http://covithelp.16mb.com/mobile_pass.php',mobiPass).subscribe((res: Response)=>{
+    return this.http.post('http://covithelp.16mb.com/WebPortalPhp/mobile_pass.php',mobiPass).subscribe((res: Response)=>{
     });
   }
 
   public addMob(mobiPass)
   {
-    return this.http.post('http://covithelp.16mb.com/mobile.php',mobiPass).subscribe((res: Response)=>{
+    return this.http.post('http://covithelp.16mb.com/WebPortalPhp/mobile.php',mobiPass).subscribe((res: Response)=>{
+    });
+  }
+
+  public enterprisedata(enterData)
+  {
+    return this.http.post('http://covithelp.16mb.com/WebPortalPhp/enterprise.php',enterData).subscribe((res: Response)=>{
+    });
+  }
+
+  public fethMobpass(mobiPass)
+  {
+    return this.http.post('http://covithelp.16mb.com/WebPortalPhp/fetch.php',mobiPass).subscribe((res: Response)=>{
     });
   }
 
